ed2a2bc731fe04751be3d0d33d7ab223c3e30ded
Gregório Neto <igregoriodoneto@outlook.com>
2022-01-27 14:46:34 -0300

    First commit

---
+++ src/test/java/com/greg/userdept/UserdeptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greg.userdept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdeptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
